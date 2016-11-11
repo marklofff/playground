a4c81c864fcb41629577af4194c8750fe233ac10
Robert Peterson <robert.peterson@gmail.com>
2016-11-10 17:48:32 -0800

    extract mutations

--- src/store/index.js
+++ src/store/index.js
@@ -2,34 +2,19 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import * as getters from './getters'
 import * as actions from './actions'
+import mutations from './mutations'
 import * as types from './mutation-types.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  actions,
-  getters,
   state: {
     socket: null,
     messages: [],
     labels: [],
     label: 'All'
   },
-  mutations: {
-    [types.SET_LABEL] (state, label) {
-      state.label = label
-    },
-    [types.SET_MESSAGES] (state, messages) {
-      state.messages = messages
-    },
-    [types.SET_LABELS] (state, labels) {
-      state.labels = labels
-    },
-    [types.SET_SOCKET] (state, socket) {
-      state.socket = socket
-    },
-    [types.ADD_MESSAGE] (state, message) {
-      state.messages.unshift(message)
-    }
-  }
+  actions,
+  getters,
+  mutations: mutations
 })
---
+++ src/store/mutations.js
@@ -0,0 +1,19 @@
+import * as types from './mutation-types'
+
+export default {
+  [types.SET_LABEL] (state, label) {
+    state.label = label
+  },
+  [types.SET_MESSAGES] (state, messages) {
+    state.messages = messages
+  },
+  [types.SET_LABELS] (state, labels) {
+    state.labels = labels
+  },
+  [types.SET_SOCKET] (state, socket) {
+    state.socket = socket
+  },
+  [types.ADD_MESSAGE] (state, message) {
+    state.messages.unshift(message)
+  }
+}
