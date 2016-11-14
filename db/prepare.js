ed63d2f0f55d16edfe123ed570109cae9886e442
Robert Peterson <robert.peterson@gmail.com>
2016-11-13 16:14:05 -0800

    db prep

--- db/prepare.js
+++ db/prepare.js
@@ -8,23 +8,23 @@ const r = require('rethinkdbdash')()
 //   process.exit()
 // })
 
-r.db(DATABASE).table("messages").delete().run().then(() => {
-  process.exit()
-})
-
-// r.dbCreate(DATABASE).run().then(()=> {
-//   console.log('created the database')
-//
-//   r.db(DATABASE).tableCreate("messages").run().then(()=> {
-//     console.log('created the messages table')
-//
-//     r.db(DATABASE).table("messages").indexCreate("created_at").run().then(()=> {
-//       console.log('add index for created_at')
-//
-//       r.db(DATABASE).table("messages").indexCreate("label").run().then(()=> {
-//         console.log('add index for label')
-//         process.exit()
-//       })
-//     })
-//   })
+// r.db(DATABASE).table("messages").delete().run().then(() => {
+//   process.exit()
 // })
+
+r.dbCreate(DATABASE).run().then(()=> {
+  console.log('created the database')
+
+  r.db(DATABASE).tableCreate("messages").run().then(()=> {
+    console.log('created the messages table')
+
+    r.db(DATABASE).table("messages").indexCreate("created_at").run().then(()=> {
+      console.log('add index for created_at')
+
+      r.db(DATABASE).table("messages").indexCreate("label").run().then(()=> {
+        console.log('add index for label')
+        process.exit()
+      })
+    })
+  })
+})
